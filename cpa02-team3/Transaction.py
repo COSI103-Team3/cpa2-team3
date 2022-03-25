5a576dbf91b5b1eb00b386a1f380c9d684065e0c
Avaharnick <64444623+Avaharnick@users.noreply.github.com>
2022-03-24 20:33:10 -0400

    Update Transaction.py
    Select month method

--- cpa02-team3/Transaction.py
+++ cpa02-team3/Transaction.py
@@ -88,3 +88,13 @@ class Transaction():
         con.commit()
         con.close()
         return to_cat_dict_list(tuples)
+
+#Ava
+    def select_month(self,month):
+        con= sqlite3.connect(self.dbfile)
+        cur = con.cursor()
+        cur.execute("SELECT rowid,* from transactions where strftime('%m', date)=(?)",(month,))
+        tuples = cur.fetchall()
+        con.commit()
+        con.close()
+        return to_cat_dict_list(tuples)
